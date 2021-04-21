33077047937bcfcc51008de1abffef44c37af653
Eric <Eric@ERIC-LAPTOP>
2021-04-21 21:29:01 +0800

    初步规范化

--- src/main/java/com/hhu/imis/App.java
+++ src/main/java/com/hhu/imis/App.java
@@ -1,5 +1,7 @@
 package com.hhu.imis;
 
+import java.util.Date;
+
 import com.alibaba.fastjson.JSONObject;
 
 /**
@@ -8,11 +10,13 @@ import com.alibaba.fastjson.JSONObject;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
         Config config = new Config();
         System.out.println(config.dbUrl);
-
+        ToDB db = new ToDB("mysql://www.hzeen.cn:3306/project", "project", "projectproject123");
+        
+        db.addRecord(new Tag("111", "A1", new Date(), "13"));
         try{
             JSONObject obj = RESTful.Restful("https://www.hzeen.cn/DemoData.txt", "city=1&key=8c17a52abbb71895e1efa8c1b3893573");
             System.out.println(obj.get("reason"));
